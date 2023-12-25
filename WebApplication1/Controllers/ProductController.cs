a1f2d714e4174e268f4a056cf2fb18f11f458808
amercado <arnold.mercado@hotmail.com>
2023-12-25 09:04:49 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ICollection<Catalog> _catalogs = new List<Catalog>();
+        public ProductController()
+        {
+            _catalogs.Add(new Catalog
+            {
+                Id = Guid.NewGuid(),
+                Description = "Test",
+                Price = 2.3m
+            });
+        }
+        public IActionResult Index()
+        {
+          
+
+
+            return View(_catalogs);
+        }
+    }
+}
---
+++ WebApplication1/Models/Catalog.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Catalog
+    {
+        public Guid Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public string Category { get; set; } = "";
+        public decimal Price { get; set; }
+
+
+    }
+}
